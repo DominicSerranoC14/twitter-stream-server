1f68a6e38da42f16996276abfc4ffc0d150b7455
Dominic Serrano <dmsrojo@gmail.com>
2017-08-31 16:07:06 -0500

    Added module that will ping the dyno to keep it from idling while the stream is open.

--- manager/seconds.js
+++ manager/seconds.js
@@ -1,16 +1,23 @@
 'use strict';
 
+const pingServer = require('./pingDyno.js');
+
 const timer = {
     seconds: 0,
 
     increment: (stream) => {
         timer.seconds++;
 
+        // Ping the server every 60 seconds to keep dyno from idling. This will only happen if a stream is open.
+        if (timer.seconds === 60) {
+            pingServer();
+        }
+
         // If 180 seconds have passed, kill the stream
         if (timer.seconds === 180) {
             stream.destroy();
         }
-    }
+    },
 };
 
 module.exports = timer;
