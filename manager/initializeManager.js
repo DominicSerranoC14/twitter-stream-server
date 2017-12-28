0ad34a7406ac520e6bf0db9e5108a3470ac95417
Dominic Serrano <dmsrojo@gmail.com>
2017-12-27 20:07:17 -0600

    Added logic for creating multiple streams. Currently testing

--- manager/initializeManager.js
+++ manager/initializeManager.js
@@ -1,51 +1,58 @@
 'use strict';
 
-const state = require('./streamState.js');
+const { createNewStreamObject, streams } = require('./streamState.js');
 const streamEventHandler = require('./streamEventHandler.js');
 
 module.exports = (socket, manager) => {
     console.log(`Client connected to server: ${socket.id}`);
 
     // Add newly connected socket to a room
-    socket.join('main-stream');
+    // socket.join('main-stream');
 
     // Tell each client that there is a stream active and disable the stream button
-    manager.to('main-stream').emit('stream-active', state.isActive);
+    // manager.to('main-stream').emit('stream-active', state.isActive);
 
     // If options exist when a socket joins, send the options and stream start time to the client
-    if (state.options && state.isActive) {
-        socket.emit('stream-options', state.options);
-    }
+    // if (state.options && state.isActive) {
+    //     socket.emit('stream-options', state.options);
+    // }
 
     // Event listners on the worker socket are set here.
     // TODO This should be broken out into another module
     socket.on('set-options', (options) => {
-        if (state.isActive) {
-            socket.emit('user-error', 'You cannot set a streams options after the stream has started.');
+        if (Object.keys(streams).length === 2) {
+            socket.emit('user-error', 'The maximum amount of streams are open. You can join an active stream.');
             return;
         }
 
-        state.options = options;
+        // Create a new stream object
+        createNewStreamObject(socket.id);
+        // Set the streams options
+        streams[socket.id].options = options;
     });
 
     socket.on('start-stream', () => {
-        if (!state.options) {
-            socket.emit('user-error', 'You must save stream options before starting a stream.');
+        // If there are more than two active streams do not let the user create a new stream
+        // TODO this can be improved by calculating whether or not enough time has passed between creating streams to not hit a 420 from Twitter
+        if (!streams[socket.id]) {
+            socket.emit('user-error', 'Save your options before starting a stream.');
             return;
         }
 
+        // Create a new room with the id of the socket that started the stream
+        socket.join(socket.id);
+
+        // Initialize the stream and the event listeners on this newly created stream
         streamEventHandler(socket, manager);
     });
 
     socket.on('stop-stream', () => {
-        if (!state.stream) return;
-
         console.log('Attempting to destroy stream...');
         // Must check for development, using two different event emitters
         if (process.env.NODE_ENV === 'development') {
-            state.stream.emit('end');
+            streams[socket.id].stream.emit('end');
         } else {
-            state.stream.destroy();
+            streams[socket.id].stream.destroy();
         }
     });
 
--- manager/streamEventHandler.js
+++ manager/streamEventHandler.js
@@ -2,64 +2,56 @@
 
 const { startStream } = require('./start-stream.js');
 const { startDevStream } = require('./developmentStream.js');
-const state = require('./streamState.js');
+const { streams } = require('./streamState.js');
 const timer = require('./seconds.js');
 const formatStatus = require('./formatStatus.js');
 
 const statusEvent = (data, socket, manager) => {
     console.log('Sending data to room');
-    manager.to('main-stream').emit('received-status', formatStatus(data));
+    manager.to(socket.id).emit('received-status', formatStatus(data));
     timer.seconds = 0;
 };
 
 // Can emit to individual socket and the manager with these params
-module.exports = (socket, manager) => {
-    // Do not start a stream if there currently is one
-    if (state.stream !== null) {
-        state.isActive = true;
-        socket.emit('stream-active', state.isActive);
-        return;
-    }
-    
+module.exports = (socket, manager) => {    
     // Start the stream and store the streamInterval to be cleared later
     if (process.env.NODE_ENV === 'development') {
         console.log('Starting development stream...');
-        state.streamInterval = startDevStream(socket, manager);
+        startDevStream(socket, manager);
     } else {
         console.log('Starting production stream...');
-        state.streamInterval = startStream(socket, manager);
+        startStream(socket, manager);
     }
     
-    // Set the state to active, and create a createdAt time stamp
-    state.isActive = true;
-    manager.to('main-stream').emit('stream-active', state.isActive);
+    // Set the stream to active
+    streams[socket.id].isActive = true;
+    manager.to(socket.id).emit('stream-active', streams[socket.id].isActive);
+    // Give the stream a timestamp to calculate duration on the client
+    streams[socket.id].createdAt = new Date();
 
-    // Add timestamp to stream object
-    state.options.createdAt = new Date();
-
-    state.stream.on('data', (data) => {
-        // TODO check the room to see if there are any sockets connected
-        // console.log(manager.sockets.adapter.rooms['main-stream']);
-        // TODO if there are no sockets in the room it may not emit, need to test
+    // Set event listener for data being sent to the stream
+    streams[socket.id].stream.on('data', data => {
         statusEvent(data, socket, manager);
     });
 
     // Log and emit errors to client
-    state.stream.on('error', (error) => {
+    streams[socket.id].stream.on('error', (error) => {
         console.log('Error', error);
-        socket.emit('stream-error', error);
+        manager.to(socket.id).emit('stream-error', error);
     });
 
-    state.stream.on('end', (response) => {
+    streams[socket.id].stream.on('end', (response) => {
         // Clear the counter interval
-        clearInterval(state.streamInterval);
+        clearInterval(streams[socket.id].streamInterval);
         // Reset the stream object
-        state.flush();
+        streams[socket.id].flush();
         // Reset the client to an inactive state
-        socket.emit('stream-active', state.isActive);
+        manager.to(socket.id).emit('stream-active', streams[socket.id].isActive);
         // Emit a 'stream-closed' event to the worker-socket connected
-        socket.emit('stream-closed');
+        manager.to(socket.id).emit('stream-closed');
+        // Remove stream object from streams collection
+        delete streams[socket.id];        
 
-        console.log('Stream has ended');
+        console.log('Stream has ended and been removed');
     });
 };   
