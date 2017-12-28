0cb701250cdb296b3dcace0d7c7f1c89323374d9
Dominic Serrano <dmsrojo@gmail.com>
2017-12-27 18:53:02 -0600

    Added the flush function back to the stream object.

--- manager/streamState.js
+++ manager/streamState.js
@@ -2,7 +2,14 @@
 
 // TODO freeze each stream object to ensure these options can only be set
 module.exports = {
+    flush() {
+        this.isActive = false;
+        this.options = null;
+        this.stream = null;
+        this.streamInterval = null;
+    },
     isActive: false,
     options: null,
     stream: null,
+    streamInterval: null,
 };
